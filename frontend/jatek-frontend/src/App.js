ec5a8f2e5646cab88ddeedc00d80a820a73876e2
Várbíró Nándor <varbiro.nandor@gmail.com>
2025-11-16 15:02:43 +0100

    fix backend, change port to 5000

--- frontend/jatek-frontend/src/App.js
+++ frontend/jatek-frontend/src/App.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API = "http://localhost:3000";
+const API = "http://localhost:5000";
 
 function App() {
   const [jatekok, setJatekok] = useState([]);
